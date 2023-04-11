c49a08dfe0c5cc3690bad6073fd04dfe93979a92
mahaUser2 <75357482+mahaUser2@users.noreply.github.com>
2023-04-11 21:29:46 +0200

    Required files added

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+//
+// FÆLLES
+//
+
+// Vi skal anvende data herfra: https://randomuser.me/
+
+// Læs dokumentation og find ud af hvordan man får en user (5 min eller indtil en har et svar)
+
+// Find ud af hvordan man får 50 users (5 min eller indtil en har et svar)
+
+// Fetch og log dem med fetch
+
+// Prøv nu at kopiere og omskrive det til async/await (Mark viser hvordan)
+
+//
+// I grupperne
+//
+
+// Prøv nu at anvende document.createElement(), .classlist.add(), .textContent og append() til at skabe en artikel med følgende struktur per user i results og append i .result:
+{
+  /* <article>
+  <h4 class="title">NAVN</h4>
+  <img src="URL" alt="ALT" />
+</article>; */
+}
+
+// Prøv nu at udkommentere ovenfor og anvend istedet .innerHTML med strukturen fra index.html. Igen en per bruger i results
+
+//
+// FÆLLES
+//
+
+// Hvis vi kan nå det: implementér filtrering i form af en input[type="search"]
