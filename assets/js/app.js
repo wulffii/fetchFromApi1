2992a7adbd0ab4cc3a7b53cd232c8dd7ca907aae
wulffii <christofferw69@gmail.com>
2025-03-24 13:30:55 +0100

    Fetched Users

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,30 @@
 
 // Find ud af hvordan man får 50 users (5 min eller indtil en har et svar)
 
+const basePoint = "https://randomuser.me/api/"
+const limitResults ="?results=50&nat=dk"
+
 // Fetch og log dem med fetch
+// fetch(basePoint + limitResults)
+// .then(res => {
+//   return res.json()
+// })
+// .then(data => console.log(data))
+// .catch(err => console.log("FEJL!!!!", err))
+
+// Her er det samme bare med async/await syntax
+
+async function fetchUsers() {
+  try {
+    const res = await fetch(basePoint + limitResults);
+    const data = await res.json();
+    renderUsers(data)
+    
+  } catch (error) {
+    console.log("FEJL", error)
+  }
+}
+fetchUsers();
 
 // Prøv nu at kopiere og omskrive det til async/await (Mark viser hvordan)
 
@@ -17,12 +40,32 @@
 //
 
 // Prøv nu at anvende document.createElement(), .classlist.add(), .textContent og append() til at skabe en artikel med følgende struktur per user i results og append i .result:
-{
+function renderUsers(listOfUsers){
+  console.log(listOfUsers)
+
+  const userContainer = document.querySelector(".users")
+  
+  listOfUsers.results.forEach(user => {
+    const article = document.createElement("article");
+  
+    const header = document.createElement("h4");
+    header.classList.add("title")
+    header.textContent =user.name.first;
+    const img = document.createElement("img")
+    img.src = user.picture.large;
+  
+  
+ article.append(header,img)
+ userContainer.append(article)
+  
+  })
+}
+
+
   /* <article>
   <h4 class="title">NAVN</h4>
   <img src="URL" alt="ALT" />
 </article>; */
-}
 
 // Prøv nu at udkommentere ovenfor og anvend istedet .innerHTML med strukturen fra index.html. Igen en per bruger i results
 
